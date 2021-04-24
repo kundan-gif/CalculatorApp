9008c9aad2d06772f695d62fbf7cc19e6e574bf5
Kundan Kumar <kundan211097@gmail.com>
2021-04-25 01:22:59 +0530

    Implemented all functionality of calculator

--- calculator/app/src/main/java/com/example/calculator/MainActivity.kt
+++ calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -15,34 +15,110 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-    fun onDigit(view:View){
+
+    fun onDigit(view: View) {
 
         tvInput.append((view as Button).text)
 
         lastNumeric = true
     }
-    fun onOperator(view:View){
+
+    fun onOperator(view: View) {
         if (lastNumeric && !isOperatorAdded(tvInput.text.toString())) {
             tvInput.append((view as Button).text)
             lastNumeric = false
             lastDot = false
         }
     }
-    fun onEqual(view:View){
-        Toast.makeText(this,"Equal clicked",Toast.LENGTH_SHORT).show()
+
+    fun onEqual(view: View) {
+
+        if (lastNumeric) {
+
+            var value = tvInput.text.toString()
+            var prefix = ""
+            try {
+
+
+                if (value.startsWith("-")) {
+                    prefix = "-"
+                    value = value.substring(1);
+                }
+
+
+                if (value.contains("/")) {
+
+                    val splitedValue = value.split("/")
+
+                    var one = splitedValue[0] // Value One
+                    val two = splitedValue[1] // Value Two
+
+                    if (!prefix.isEmpty()) {
+                        one = prefix + one
+                    }
+
+
+                    tvInput.text = removeZeroAfterDot((one.toDouble() / two.toDouble()).toString())
+                } else if (value.contains("*")) {
+
+                    val splitedValue = value.split("*")
+
+                    var one = splitedValue[0]
+                    val two = splitedValue[1]
+
+                    if (!prefix.isEmpty()) {
+                        one = prefix + one
+                    }
+
+
+                    tvInput.text = removeZeroAfterDot((one.toDouble() * two.toDouble()).toString())
+                } else if (value.contains("-")) {
+
+
+                    val splitedValue = value.split("-")
+
+                    var one = splitedValue[0]
+                    val two = splitedValue[1]
+
+                    if (!prefix.isEmpty()) {
+                        one = prefix + one
+                    }
+
+
+                    tvInput.text = removeZeroAfterDot((one.toDouble() - two.toDouble()).toString())
+                } else if (value.contains("+")) {
+
+                    val splitedValue = value.split("+")
+
+                    var one = splitedValue[0]
+                    val two = splitedValue[1]
+
+                    if (!prefix.isEmpty()) {
+                        one = prefix + one
+                    }
+
+                    tvInput.text = removeZeroAfterDot((one.toDouble() + two.toDouble()).toString())
+                }
+            } catch (e: ArithmeticException) {
+                e.printStackTrace()
+            }
+        }
     }
-    fun onClear(view:View){
+
+    fun onClear(view: View) {
         tvInput.text = ""
         lastNumeric = false
         lastDot = false
     }
-    fun onDecimalPoint(view:View){
+
+    fun onDecimalPoint(view: View) {
         if (lastNumeric && !lastDot) {
             tvInput.append(".")
             lastNumeric = false
             lastDot = true
         }
     }
+
     private fun isOperatorAdded(value: String): Boolean {
 
         return if (value.startsWith("-")) {
@@ -53,5 +129,16 @@ class MainActivity : AppCompatActivity() {
                     || value.contains("-")
                     || value.contains("+"))
         }
+    }
+
+    private fun removeZeroAfterDot(result: String): String {
+
+        var value = result
+
+        if (result.contains(".0")) {
+            value = result.substring(0, result.length - 2)
+        }
+
+        return value
     }
 }
