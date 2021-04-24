89317614e5903d312582ce557475d0377837ded4
Kundan Kumar <kundan211097@gmail.com>
2021-04-25 00:49:47 +0530

    Implemented the onclick of digit and operator

--- calculator/app/src/main/java/com/example/calculator/MainActivity.kt
+++ calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -3,26 +3,55 @@ package com.example.calculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var lastNumeric: Boolean = false
+
+    var lastDot: Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
     fun onDigit(view:View){
-        Toast.makeText(this,"Digit clicked",Toast.LENGTH_SHORT).show()
+
+        tvInput.append((view as Button).text)
+
+        lastNumeric = true
     }
     fun onOperator(view:View){
-        Toast.makeText(this,"Operator clicked",Toast.LENGTH_SHORT).show()
+        if (lastNumeric && !isOperatorAdded(tvInput.text.toString())) {
+            tvInput.append((view as Button).text)
+            lastNumeric = false
+            lastDot = false
+        }
     }
     fun onEqual(view:View){
         Toast.makeText(this,"Equal clicked",Toast.LENGTH_SHORT).show()
     }
     fun onClear(view:View){
-        Toast.makeText(this,"Clear clicked",Toast.LENGTH_SHORT).show()
+        tvInput.text = ""
+        lastNumeric = false
+        lastDot = false
     }
     fun onDecimalPoint(view:View){
-        Toast.makeText(this,"Decimal clicked",Toast.LENGTH_SHORT).show()
+        if (lastNumeric && !lastDot) {
+            tvInput.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
+    }
+    private fun isOperatorAdded(value: String): Boolean {
+
+        return if (value.startsWith("-")) {
+            false
+        } else {
+            (value.contains("/")
+                    || value.contains("*")
+                    || value.contains("-")
+                    || value.contains("+"))
+        }
     }
 }
